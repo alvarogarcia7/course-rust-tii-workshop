485b5ad5045ca59f1740bc10a46daba7d2a8c3ef
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-06 01:44:18 +0400

    perimeter for circle

--- p32/src/shapes.rs
+++ p32/src/shapes.rs
@@ -7,18 +7,33 @@
 // – biggest_area: accepts two shapes (with potentially different types) and returns reference to a shape with the biggest area
 // – print_properties: prints name, area, and perimeter of the shape
 
-trait Shape {
-    const NAME: &'static str;
+use std::f64::consts::PI;
+
+pub trait DecimalPerimeter {
+    fn perimeter(&self) -> f64;
+}
+
+pub trait WholePerimeter {
     fn perimeter(&self) -> u64;
 }
+
+#[allow(dead_code)]
+pub trait ShapeName {
+    const NAME: &'static str;
+}
+
+#[allow(dead_code)]
+pub trait WholePerimeterShape: ShapeName + WholePerimeter {}
 
 struct Square {
     sides: u64,
 }
 
-impl Shape for Square {
+impl ShapeName for Square {
     const NAME: &'static str = "Square";
+}
 
+impl WholePerimeter for Square {
     fn perimeter(&self) -> u64 {
         self.sides * self.sides
     }
@@ -35,9 +50,11 @@ struct Rectangle {
     height: u64,
 }
 
-impl Shape for Rectangle {
+impl ShapeName for Rectangle {
     const NAME: &'static str = "Square";
+}
 
+impl WholePerimeter for Rectangle {
     fn perimeter(&self) -> u64 {
         self.width * self.height
     }
@@ -49,9 +66,33 @@ impl Rectangle {
     }
 }
 
+struct Circle {
+    radius: u64,
+}
+
+impl ShapeName for Circle {
+    const NAME: &'static str = "Circle";
+}
+
+impl DecimalPerimeter for Circle {
+    fn perimeter(&self) -> f64 {
+        self.radius
+            .checked_mul(self.radius)
+            .map(|x| (x as f64) * (PI))
+            .unwrap()
+    }
+}
+
+impl Circle {
+    pub fn new(radius: u64) -> Self {
+        Self { radius }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::f64::consts::PI;
 
     #[test]
     pub fn calculate_perimeter_for_square() {
@@ -69,5 +110,14 @@ mod tests {
         let actual = shape.perimeter();
 
         assert_eq!(actual, 8 * 6)
+    }
+
+    #[test]
+    pub fn calculate_perimeter_for_circle() {
+        let shape = Circle::new(3);
+
+        let actual = shape.perimeter();
+
+        assert_eq!(actual, PI * 3f64 * 3f64);
     }
 }
