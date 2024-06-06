d6cb928e96711f05851069e8a65b27bcfc9d75ca
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-07 03:08:40 +0400

    move test data into the test module

--- p22/src/fibonacci.rs
+++ p22/src/fibonacci.rs
@@ -46,56 +46,55 @@ pub fn fibonacci_non_recursive(n: u32) -> u64 {
     number
 }
 
-// TODO: is this configuration useful?
-#[cfg(test)]
-// Source: https://oeis.org/A000045
-static FIBONACCI_NUMBERS: [u64; 41] = [
-    0,
-    1,
-    1,
-    2,
-    3,
-    5,
-    8,
-    13,
-    21,
-    34,
-    55,
-    89,
-    144,
-    233,
-    377,
-    610,
-    987,
-    1597,
-    2584,
-    4181,
-    6765,
-    10946,
-    17711,
-    28657,
-    46368,
-    75025,
-    121393,
-    196418,
-    317811,
-    514229,
-    832040,
-    1346269,
-    2178309,
-    3524578,
-    5702887,
-    9227465,
-    14930352,
-    24157817,
-    39088169,
-    63245986,
-    102334155u64,
-];
-
 #[cfg(test)]
 mod tests {
+    // Source: https://oeis.org/A000045
+    static FIBONACCI_NUMBERS: [u64; 41] = [
+        0,
+        1,
+        1,
+        2,
+        3,
+        5,
+        8,
+        13,
+        21,
+        34,
+        55,
+        89,
+        144,
+        233,
+        377,
+        610,
+        987,
+        1597,
+        2584,
+        4181,
+        6765,
+        10946,
+        17711,
+        28657,
+        46368,
+        75025,
+        121393,
+        196418,
+        317811,
+        514229,
+        832040,
+        1346269,
+        2178309,
+        3524578,
+        5702887,
+        9227465,
+        14930352,
+        24157817,
+        39088169,
+        63245986,
+        102334155u64,
+    ];
     mod recursive {
+        use crate::fibonacci::tests::FIBONACCI_NUMBERS;
+
         use super::super::*;
 
         #[test]
@@ -122,6 +121,8 @@ mod tests {
     }
 
     mod non_recursive {
+        use crate::fibonacci::tests::FIBONACCI_NUMBERS;
+
         use super::super::*;
 
         #[test]
