b86146b7fdf002b0f1032a3af88fdd39d413ff5f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-07 03:06:43 +0400

    review: benches - re: criterion
    Comment: Usually you benchmark function with a single parameter. You then can introduce separate benchmarks functions for different parameters, e.g. bench_non_recursive_10, bench_non_recursive_20, bench_non_recursive_30, etc. Or you can use criterion to automate this.
    
    Done:
    1. Split benches into a specific case
    2. Use children modules

--- p22/benches/benches.rs
+++ p22/benches/benches.rs
@@ -2,27 +2,52 @@
 
 extern crate test;
 
-#[cfg(test)]
-mod benchmarks {
+mod non_recursive {
+    use p22::fibonacci::fibonacci_non_recursive;
     use test::Bencher;
-
-    use p22::fibonacci::{fibonacci_non_recursive, fibonacci_recursive};
-
     #[bench]
-    fn non_recursive(b: &mut Bencher) {
+    fn case_10(b: &mut Bencher) {
         b.iter(|| {
-            for i in 1..=30 {
-                fibonacci_non_recursive(std::hint::black_box(i));
-            }
+            fibonacci_non_recursive(std::hint::black_box(10));
         });
     }
 
     #[bench]
-    fn recursive(b: &mut Bencher) {
+    fn case_20(b: &mut Bencher) {
         b.iter(|| {
-            for i in 1..=30 {
-                fibonacci_recursive(std::hint::black_box(i));
-            }
+            fibonacci_non_recursive(std::hint::black_box(20));
+        });
+    }
+
+    #[bench]
+    fn case_30(b: &mut Bencher) {
+        b.iter(|| {
+            fibonacci_non_recursive(std::hint::black_box(30));
+        });
+    }
+}
+
+mod recursive {
+    use p22::fibonacci::fibonacci_recursive;
+    use test::Bencher;
+    #[bench]
+    fn case_10(b: &mut Bencher) {
+        b.iter(|| {
+            fibonacci_recursive(std::hint::black_box(10));
+        });
+    }
+
+    #[bench]
+    fn case_20(b: &mut Bencher) {
+        b.iter(|| {
+            fibonacci_recursive(std::hint::black_box(20));
+        });
+    }
+
+    #[bench]
+    fn case_30(b: &mut Bencher) {
+        b.iter(|| {
+            fibonacci_recursive(std::hint::black_box(30));
         });
     }
 }
