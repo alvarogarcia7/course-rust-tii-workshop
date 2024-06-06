c36d41b0343ff0f9e53e8eb3ee3b7ed5498d2f82
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-06 23:39:16 +0400

    refactor: rename typo in fahrenheit (farenheit)

--- p22/src/lib.rs
+++ p22/src/lib.rs
@@ -1,11 +1,11 @@
-fn celsius2farenheit(celsius: i32) -> i32 {
+fn celsius2fahrenheit(celsius: i32) -> i32 {
     // Careful with https://rust-lang.github.io/rust-clippy/master/index.html#/identity_op
     ((celsius * 9) / 5) + 32
 }
 
-fn farenheit2celsius(farenheit: i32) -> i32 {
+fn fahrenheit2celsius(fahrenheit: i32) -> i32 {
     // Careful with https://rust-lang.github.io/rust-clippy/master/index.html#/identity_op
-    (5 * (farenheit - 32)) / 9
+    (5 * (fahrenheit - 32)) / 9
 }
 
 #[cfg(test)]
@@ -16,7 +16,7 @@ mod tests {
     fn loop_from_celsius() {
         let same_celsius_value = -5;
         assert_eq!(
-            farenheit2celsius(celsius2farenheit(same_celsius_value)),
+            fahrenheit2celsius(celsius2fahrenheit(same_celsius_value)),
             same_celsius_value
         );
     }
@@ -25,7 +25,7 @@ mod tests {
     fn loop_from_farenheit() {
         let same_fahrenheit_value = 212;
         assert_eq!(
-            celsius2farenheit(farenheit2celsius(same_fahrenheit_value)),
+            celsius2fahrenheit(fahrenheit2celsius(same_fahrenheit_value)),
             same_fahrenheit_value
         );
     }
@@ -35,14 +35,14 @@ mod tests {
         // Source: https://fr.farnell.com/en-FR/convertisseur-temperature
         let equivalences_fahrenheit_and_celsius = [(23, -5), (194, 90), (212, 100), (392, 200)];
         for equivalence in equivalences_fahrenheit_and_celsius {
-            let actual_celsius = farenheit2celsius(equivalence.0);
+            let actual_celsius = fahrenheit2celsius(equivalence.0);
             assert_eq!(
                 equivalence.1, actual_celsius,
                 "error: fahrenheit {} is not celsius {}. actual: {}",
                 equivalence.0, equivalence.1, actual_celsius
             );
 
-            let actual_fahrenheit = celsius2farenheit(equivalence.1);
+            let actual_fahrenheit = celsius2fahrenheit(equivalence.1);
             assert_eq!(
                 equivalence.1, actual_celsius,
                 "error: celsius {} is not fahrenheit {}. actual: {}",
@@ -50,11 +50,11 @@ mod tests {
             );
 
             assert_eq!(
-                celsius2farenheit(farenheit2celsius(equivalence.0)),
+                celsius2fahrenheit(fahrenheit2celsius(equivalence.0)),
                 equivalence.0
             );
             assert_eq!(
-                farenheit2celsius(celsius2farenheit(equivalence.1)),
+                fahrenheit2celsius(celsius2fahrenheit(equivalence.1)),
                 equivalence.1
             );
         }
