831b9d817ed8d7fa9afdb0b8c8e1c7450e5bc705
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-06 12:07:04 +0400

    move the computation of ordinal to a method

--- p42/src/song.rs
+++ p42/src/song.rs
@@ -1,6 +1,7 @@
 struct SongIter {
     current: usize,
     storage: Vec<String>,
+    computed_storage: Vec<String>,
 }
 
 impl SongIter {
@@ -22,7 +23,50 @@ impl SongIter {
                 "On the eleventh day of Christmas, my true love sent to me Eleven pipers piping, Ten lords a-leaping, Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
                 "On the twelfth day of Christmas, my true love sent to me Twelve drummers drumming, Eleven pipers piping, Ten lords a-leaping, Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree!".to_string(),
             ],
+            computed_storage:
+            vec![
+                "A partridge in a pear tree.".to_string(),
+                "Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Ten lords a-leaping, Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Eleven pipers piping, Ten lords a-leaping, Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree.".to_string(),
+                "Twelve drummers drumming, Eleven pipers piping, Ten lords a-leaping, Nine ladies dancing, Eight maids a-milking, Seven swans a-swimming, Six geese a-laying, Five gold rings, Four calling birds, Three French hens, Two turtle doves, And a partridge in a pear tree!".to_string(),
+            ],
         }
+    }
+
+    fn nth_day(&self, day: usize) -> String {
+        let ordinal = match day + 1 {
+            1 => "first".to_string(),
+            2 => "second".to_string(),
+            3 => "third".to_string(),
+            4 => "fourth".to_string(),
+            5 => "fifth".to_string(),
+            6 => "sixth".to_string(),
+            7 => "seventh".to_string(),
+            8 => "eighth".to_string(),
+            9 => "ninth".to_string(),
+            10 => "tenth".to_string(),
+            11 => "eleventh".to_string(),
+            12 => "twelfth".to_string(),
+            _ => todo!(),
+        };
+
+        format!(
+            "On the {} day of Christmas, my true love sent to me",
+            ordinal
+        )
+        .to_string()
+    }
+
+    pub fn compute(&self, day: usize) -> String {
+        format!("{} {}", self.nth_day(day), self.computed_storage[day])
     }
 }
 
@@ -31,7 +75,9 @@ impl Iterator for SongIter {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.current < (self.storage.len()) {
+            let computed = Some(self.compute(self.current));
             let return_value = Some(self.storage[self.current].to_string());
+            assert_eq!(computed, return_value);
             self.current += 1;
             return return_value;
         }
