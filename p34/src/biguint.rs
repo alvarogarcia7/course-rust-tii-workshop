9e9edbf6858c8d67b5ec0cc7b7015903d2cce738
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-07 17:20:21 +0400

    refactor: move the module to the actual file

--- p34/src/biguint.rs
+++ p34/src/biguint.rs
@@ -6,6 +6,29 @@
 // ● Implement conversion methods from hex-encoded &str and to hex-
 // encoded String (be careful about endianness)
 // ● Add integration tests for the type
-pub mod biguint4096 {}
 
+#[derive(Debug, PartialEq)]
+pub struct BigUint4096 {
+    value: [u64; 64],
+}
 
+impl BigUint4096 {
+    pub fn from(initial_value: [u64; 64]) -> Self {
+        Self {
+            value: initial_value,
+        }
+    }
+    pub fn new() -> Self {
+        Self::from([0; 64])
+    }
+
+    pub fn sum(&mut self, another: &Self) {
+        self.value[63] += another.value[63];
+    }
+}
+
+impl Default for BigUint4096 {
+    fn default() -> Self {
+        Self::new()
+    }
+}
