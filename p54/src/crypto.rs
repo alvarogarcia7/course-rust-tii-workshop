d8a5c9274812d0edfa6b5652f97bb2fcf14101bb
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-10 18:33:21 +0400

    convert to Vec<u8> and add asserts

--- p54/src/crypto.rs
+++ p54/src/crypto.rs
@@ -3,14 +3,14 @@
 use aes_gcm::aead::{Aead, OsRng};
 use aes_gcm::{AeadCore, Aes256Gcm, Key, KeyInit, Nonce};
 
-pub fn encrypt(key_str: String, plaintext: String) -> String {
-    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+pub fn encrypt(key_str: &[u8; 32], plaintext: Vec<u8>) -> Vec<u8> {
+    let key = Key::<Aes256Gcm>::from_slice(key_str);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
     let cipher = Aes256Gcm::new(key);
 
     let ciphered_data = cipher
-        .encrypt(&nonce, plaintext.as_bytes())
+        .encrypt(&nonce, &*plaintext)
         .expect("failed to encrypt");
 
     // combining nonce and encrypted data together
@@ -18,24 +18,20 @@ pub fn encrypt(key_str: String, plaintext: String) -> String {
     let mut encrypted_data: Vec<u8> = nonce.to_vec();
     encrypted_data.extend_from_slice(&ciphered_data);
 
-    hex::encode(encrypted_data)
+    encrypted_data
 }
 
-pub fn decrypt(key_str: String, encrypted_data: String) -> String {
-    let encrypted_data = hex::decode(encrypted_data).expect("failed to decode hex string into vec");
-
-    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+pub fn decrypt(key_str: &[u8; 32], encrypted_data: Vec<u8>) -> Vec<u8> {
+    let key = Key::<Aes256Gcm>::from_slice(key_str);
 
     let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
     let nonce = Nonce::from_slice(nonce_arr);
 
     let cipher = Aes256Gcm::new(key);
 
-    let plaintext = cipher
+    cipher
         .decrypt(nonce, ciphered_data)
-        .expect("failed to decrypt data");
-
-    String::from_utf8(plaintext).expect("failed to convert vector of bytes to string")
+        .expect("failed to decrypt data")
 }
 
 #[cfg(test)]
--- p54/src/main.rs
+++ p54/src/main.rs
@@ -1,15 +1,29 @@
 use p54::crypto::{decrypt, encrypt};
 
 fn main() {
-    let plaintext = "backendengineer.io".to_string();
+    let p_s = "backendengineer.io AND MORE DATA IS ALSO POSSIBLE";
+    let plaintext_string = p_s.as_bytes();
+    let plaintext = plaintext_string.to_vec();
 
-    let key_str = "thiskeystrmustbe32charlongtowork".to_string();
+    let key_as_vec = "thiskeystrmustbe32charlongtowork".as_bytes();
+    assert_eq!(key_as_vec.len(), 32);
+    let key: [u8; 32] = <[u8; 32]>::try_from(key_as_vec).unwrap();
 
-    let encrypted_data = encrypt(key_str.clone(), plaintext);
+    let encrypted_data = encrypt(&key, plaintext);
 
-    println!("encrypted_data: {:?}", encrypted_data.clone());
+    let hex_encoded_encrypted_data = hex::encode(&encrypted_data).clone();
+    println!(
+        "encrypted_data: {:?}, len = {:?}",
+        hex_encoded_encrypted_data,
+        hex_encoded_encrypted_data.len()
+    );
 
-    let original = decrypt(key_str, encrypted_data);
+    let decrypted_plaintext = decrypt(&key, encrypted_data);
 
-    println!("original: {:?}", original);
+    let as_string = String::from_utf8(decrypted_plaintext.clone())
+        .expect("failed to convert vector of bytes to string");
+    println!("original: {:?}", as_string);
+
+    assert_eq!(decrypted_plaintext, plaintext_string);
+    println!("The original and decrypted values match.")
 }
